e2b7246089a872c0a0faf8884e749ec62cce8a7e
Stefania Sferragatta <sferragatta.stefania@gmail.com>
2022-01-03 18:38:43 +0100

    Add files via upload

--- arima_cc.py
+++ arima_cc.py
@@ -7,8 +7,16 @@ import matplotlib.pyplot as plt
 import warnings
 warnings.filterwarnings('ignore')
 
-#FUNCTIONS
-def arima(train,test,p):
+def arima(train : pd.core.series.Series,test : pd.core.series.Series,p : int):
+    ''' 
+    Function that takes in input:
+    - the trainset
+    - the test set
+    - the value of p (AR)
+    
+    and defines the ARIMA model, fits it, gets the forecast.
+    It returns the predicted values, the lower and the upper series.
+    '''
     model = ARIMA(train, order=(p, 1, 1))  
     fitted = model.fit()  
 
@@ -21,7 +29,18 @@ def arima(train,test,p):
     return fc_series,lower_series,upper_series
 
 # Plot
-def plot(train,test,fc_series,lower_series,upper_series,t):
+def plot(train: pd.core.series.Series,test : np.ndarray, fc_series : pd.core.series.Series,lower_series : pd.core.series.Series,upper_series : pd.core.series.Series, t : str):
+    '''
+    Function that takes in input:
+    - the trainset
+    - the test set values
+    - the forecasted set
+    - the lower bound set
+    - the upper bound set
+    - the string containing the feature's name
+
+    Plot the forecast VS actuals for each feature with a grey shadow indicates the confidence interval.
+    '''
     plt.figure(figsize=(12,5), dpi=200)
     plt.plot(train, label='training', color = 'lightcoral')
     plt.plot(test, label='actual',color = 'peachpuff',linestyle = 'dashed')
@@ -32,7 +51,14 @@ def plot(train,test,fc_series,lower_series,upper_series,t):
     plt.show()
     return
 
-def forecast_accuracy(forecast, actual):
+def forecast_accuracy(forecast : pd.core.series.Series, actual : np.ndarray):
+    ''' 
+    Function that takes in input:
+    - forecast set
+    - actual values
+
+    and perform an evaluation of the metod computing the following measures: MAE,RMSE, correlation coefficient.
+    '''
     mae = np.mean(np.abs(forecast - actual))    # MAE
     rmse = np.mean((forecast - actual)**2)**.5  # RMSE
     corr = np.corrcoef(forecast, actual)[0,1]   # corr
