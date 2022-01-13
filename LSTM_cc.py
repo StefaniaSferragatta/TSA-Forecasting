cf39ae95c44643b2bb8e5afd27a5d8d9cc61e481
Stefania Sferragatta <sferragatta.stefania@gmail.com>
2022-01-13 19:21:58 +0100

    Add files via upload

--- LSTM_cc.py
+++ LSTM_cc.py
@@ -11,13 +11,30 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, LSTM
 from tensorflow import keras
 
-def timeseries_to_supervised(data, lag=1):
+def difference(dataset: np.narray, interval=1):
+	'''
+	Function that takes in input:
+	- the dataset's values 
+	- the grade to differenciate (in this case 1)
+
+	and differenciate the data to eliminate the seasonality and make the data stationary.
+	It returns a series of the differenciated data.
+	'''
+	diff = list()
+	for i in range(interval, len(dataset)):
+		# the first observation in the series is skipped as there is no prior observation with which to calculate a differenced value.
+		value = dataset[i] - dataset[i - interval]
+		diff.append(value)
+	return Series(diff)
+
+def timeseries_to_supervised(data: pd.core.series.Series, lag=1):
     '''
     Function that takes in input:
     - numpy array of the raw time series data 
     - the number of shifted series to create and use as inputs (lag)
 
-    It returns a dataframe '''
+    It returns a new dataframe.
+	'''
 
     df = DataFrame(data)
     columns = [df.shift(i) for i in range(1, lag+1)]
@@ -26,25 +43,20 @@ def timeseries_to_supervised(data, lag=1):
     df.fillna(0, inplace=True)
     return df
 
-def difference(dataset, interval=1):
-	diff = list()
-	for i in range(interval, len(dataset)):
-		# the first observation in the series is skipped as there is no prior observation with which to calculate a differenced value.
-		value = dataset[i] - dataset[i - interval]
-		diff.append(value)
-	return Series(diff)
-
 # invert differenced value
-def inverse_difference(history, yhat, interval=1):
+def inverse_difference(history: np.ndarray, yhat: np.ndarray, interval=1):
 	'''
 	Functions that takes in input:
+	- the raw values
+	- the predicted value
+	- the number of differenciate apply to the data
 
 	and invert the differenciating process in order to take forecasts made on the differenced 
 	series back into their original scale.
 	'''
 	return yhat + history[-interval]
 
-def scale(train, test):
+def scale(train: np.ndarray, test: np.ndarray):
 	'''
 	Function that takes in input:
 	- train dataset
@@ -64,11 +76,11 @@ def scale(train, test):
 	test_scaled = scaler.transform(test)
 	return scaler, train_scaled, test_scaled
  
-def invert_scale(scaler, X, value):
+def invert_scale(scaler: MinMaxScaler, X: np.ndarray, value: np.ndarray):
 	'''
 	Function that takes in input:
-	-
-	-
+	- the scaler used to scale the data
+	- the predicted value
 	- the forecasted value
 
 	and performs an inverse scaling for the given forecasted value.
@@ -81,13 +93,13 @@ def invert_scale(scaler, X, value):
 	return inverted[0, -1]
  
 # fit an LSTM network to training data
-def fit_lstm(train, batch_size, nb_epoch, neurons):
+def fit_lstm(train: np.ndarray, batch_size: int, nb_epoch: int, neurons: int):
 	'''
 	Function that takes in input:
 	- the trainset
-	- the batch size
-	- the number of epochs
-	- 
+	- the batch size (1)
+	- the number of epochs (10)
+	- the number of neurons (4)
 	
 	and applies the defined model to the data and return it.	
 	'''
@@ -102,28 +114,28 @@ def fit_lstm(train, batch_size, nb_epoch, neurons):
 		model.reset_states()
 	return model
  
-def forecast_lstm(model, batch_size, X):
+def forecast_lstm(model: Sequential, batch_size: int, X: np.ndarray):
 	'''
 	Function that takes in input:
 	- the model fitted
 	- the number of batch's size
 	- the values
 
-	and makes a one step forecast. It returns the forecasted values
+	and makes a one step forecast. It returns the forecasted values.
 	'''
 	X = X.reshape(1, 1, len(X))
 	yhat = model.predict(X, batch_size=batch_size)
 	return yhat[0,0]
 
-def robust_lstm(train_scaled,test_scaled,scaler,raw_values):
+def robust_lstm(train_scaled: np.ndarray,test_scaled: np.ndarray,scaler: MinMaxScaler,raw_values: np.ndarray):
 	'''
 	Function that takes in input:
-	- 
-	- 
-	- 
-	- 
+	- the train set
+	- the test set
+	- the scaler used to scale the data 
+	- the dataset's values
 
-	and perform the model fitting and the walk forward validation into a loop, doing it for 10 times.
+	and performs the model fitting and the walk forward validation into a loop, doing it for 10 times.
 	At each iteration the RMSE computed is stored and, in the end, returned.
 	It's used to plot its distribution.
 	'''	
